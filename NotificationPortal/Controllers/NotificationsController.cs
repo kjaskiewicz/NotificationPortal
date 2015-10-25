0d8f8ce24bae082ec2462a321ce0c61b0a8a1632
jaskiewicz <kjaskiewicz@gmail.com>
2015-10-25 12:05:35 -0700

    Initial commit.

---
+++ NotificationPortal/Controllers/NotificationsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotificationPortal.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        // GET: Notifications
+        public ActionResult Set()
+        {
+            return base.View();
+        }
+
+        // GET: Notifications
+        public new ActionResult View()
+        {
+            return base.View();
+        }
+
+        // GET: Notifications/Details/5
+        public ActionResult Details(int id)
+        {
+            return base.View();
+        }
+
+        // GET: Notifications/Create
+        public ActionResult Create()
+        {
+            return base.View();
+        }
+
+        // POST: Notifications/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return base.View();
+            }
+        }
+
+        // GET: Notifications/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return base.View();
+        }
+
+        // POST: Notifications/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return base.View();
+            }
+        }
+
+        // GET: Notifications/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return base.View();
+        }
+
+        // POST: Notifications/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return base.View();
+            }
+        }
+    }
+}
